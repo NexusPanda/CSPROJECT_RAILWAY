8d3235ee4cd09b41fb311b54d268a96c4a1fc665
NexusPanda <157189888+NexusPanda@users.noreply.github.com>
2025-12-25 23:34:33 +0530

    Some changes made.

---
+++ RailwayTicketBookingSystem/src/com/irctc/Main.java
@@ -0,0 +1,9 @@
+package com.irctc;
+
+import com.irctc.controller.BookingController;
+
+public class Main {
+    public static void main(String[] args) {
+        new BookingController().start();
+    }
+}
